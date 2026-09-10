9818e12c3eb9f3a6c2c66238869d0cfab903e89b
Ronald <ronaldagueropolanco@gmail.com>
2026-09-10 18:13:33 -0400

    Agrega herencia: clases Heroe y Enemigo

--- main.py
+++ main.py
@@ -6,4 +6,20 @@ class Personaje:
         self.habilidades = habilidades
 
     def atacar(self, objetivo):
-        objetivo - self.vida
+        objetivo.vida -= self.dano
+
+class Heroe(Personaje):
+    def __init__(self, nombre, vida, dano, habilidades, superpoder):
+        super().__init__(nombre, vida, dano, habilidades)
+        self.superpoder = superpoder
+
+class Enemigo(Personaje):
+    def __init__(self, nombre, vida, dano, habilidades, arma_poderosa):
+        super().__init__(nombre, vida, dano, habilidades)
+        self.arma_poderosa = arma_poderosa
+
+superman = Heroe("SuperMan", 300, 150, ["Volar", "Super Fuerza"], "super_fuerza")
+
+zod = Enemigo("Zod", 250, 125, ["Fuerza", "Resistencia"], "Pistola")
+
+superman.atacar(zod)

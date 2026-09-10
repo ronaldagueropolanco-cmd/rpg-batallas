d6b0245052c9c27a5d0f3e88cbcb526b12037ef9
Ronald <ronaldagueropolanco@gmail.com>
2026-09-10 17:43:51 -0400

    Agrega clase Personaje con metodo atacar

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hola Mundo")
+class Personaje:
+    def __init__(self, nombre, vida, dano, habilidades):
+        self.nombre = nombre
+        self.vida = vida
+        self.dano = dano
+        self.habilidades = habilidades
+
+    def atacar(self, objetivo):
+        objetivo - self.vida

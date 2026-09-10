378113c228631a405f884dc1fe770a0856d9140d
Ronald <ronaldagueropolanco@gmail.com>
2026-09-10 19:11:08 -0400

    Agrega sistema de batalla por turnos con verificacion de derrota

--- main.py
+++ main.py
@@ -22,4 +22,17 @@ superman = Heroe("SuperMan", 300, 150, ["Volar", "Super Fuerza"], "super_fuerza"
 
 zod = Enemigo("Zod", 250, 125, ["Fuerza", "Resistencia"], "Pistola")
 
-superman.atacar(zod)
+while True:
+    superman.atacar(zod)
+    if zod.vida >= 0:
+        print(f"{superman.nombre} ataca a {zod.nombre}. {zod.nombre}  le queda con {zod.vida} de vida")
+    else:
+        print(f"{zod.nombre} fue derrotado. Ganador {superman.nombre}")
+        break
+
+    zod.atacar(superman)
+    if superman.vida >= 0:
+        print(f"{zod.nombre} ataca a {superman.nombre}. {superman.nombre}  le queda con {superman.vida} de vida \n")
+    else:
+        print(f"{superman.nombre} fue derrotado. Ganador {zod.nombre}")
+        break
